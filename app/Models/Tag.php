ac4512f8ee8c9280bce9650551cfc148be4bf4ab
unknown <thousand24@outlook.fr>
2021-10-11 09:07:59 +0200

    manytomany2

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+    $post = Post::find(1);
+
+foreach ($post->tags as $tag) {
+}
+    public function posts()
+    {
+        return $this->morphedByMany(Post::class, 'taggable');
+    }
+
+    public function videos()
+    {
+        return $this->morphedByMany(Video::class, 'taggable');
+    }
+
+   
 }
